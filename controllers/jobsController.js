d734adc3bb9cfeb27659e2fc403b3d9fbf96c726
Krzysztof-K94 <krzysztofkukielka94@gmail.com>
2022-08-20 23:51:33 +0200

    add monthlyApplications backend

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import moment from 'moment';
 import Job from '../models/Job.js';
 import { StatusCodes } from "http-status-codes";
 import {BadRequestError, NotFoundError, UnauthenticatedError} from '../errors/index.js';
@@ -69,7 +70,26 @@ const showStats = async(req, res) => {
     interview: 0 || stats.interview,
     declined: 0 || stats.declined,
   };
-  const monthlyApplications = [];
+  let monthlyApplications = await Job.aggregate([
+    {$match: {createdBy : mongoose.Types.ObjectId(req.user.userId)}},
+    {$group: {
+      _id :{
+        year: {$year : '$createdAt'},
+        month: {$month: '$createdAt'},
+      },
+      count: {$sum : 1},
+    }},
+    {$sort: {'_id.year':-1, '_id.month':-1}},
+    {$limit: 6},
+  ]);
+  monthlyApplications = monthlyApplications.map((item) => {
+    const {_id:{year,month}, count} = item;
+    const date = moment()
+      .month(month-1)
+      .year(year)
+      .format('MMM Y')
+      return {date, count};
+  }).reverse();
   res.status(StatusCodes.OK).json({defaultStats, monthlyApplications});
 };
 
