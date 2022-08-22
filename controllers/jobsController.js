cfa2593b99d1ef8bfb7af43681592a9baa54666c
Krzysztof-K94 <krzysztofkukielka94@gmail.com>
2022-08-22 15:14:07 +0200

    add paggination backend

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -33,7 +33,6 @@ const getAllJobs = async(req, res) => {
   const queryObject = {
     createdBy: req.user.userId,
   }
-
   if(status !== 'all') queryObject.status = status
   if(jobType !== 'all') queryObject.jobType = jobType
   if(search) queryObject.position = {$regax: search, $option: 'i'}
@@ -44,9 +43,19 @@ const getAllJobs = async(req, res) => {
   if(sort === 'oldest') result.sort('createdAt');
   if(sort === 'a-z') result.sort('position');
   if(sort === 'z-a') result.sort('-position');
-  
+
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  result = result.skip(skip).limit(limit);
+
   const jobs = await result;
-  res.status(StatusCodes.OK).json({jobs, totalJobs: jobs.length, numOfPages: 1});
+  const totalJobs = await Job.countDocuments(queryObject);
+  const numOfPages = Math.ceil(jobsTotal/limit);
+
+  numOfPages = jobs.length/limit;
+  res.status(StatusCodes.OK).json({jobs, totalJobs, numOfPages});
 };
 
 const updateJob = async(req, res) => {
